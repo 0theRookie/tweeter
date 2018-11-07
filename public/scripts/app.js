31afb31161f77a5ef87d94130de11ca154364aac
Keishon Ross <keishonross97@gmail.com>
2018-11-07 19:18:07 +0000

    creates article element with innerText from database appended

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,15 +1,3 @@
-$(function () {
-    console.log("DOM ready");
-
-    function createTweetElement(data) {
-        
-    }
-});
-
-
-
-createTweetElement(tweetData);
-
 const tweetData = {
     "user": {
       "name": "Newton",
@@ -24,4 +12,22 @@ const tweetData = {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
     "created_at": 1461116232227
-  }
+}
+
+
+function createTweetElement(data) {
+
+    const $articleElm = $("<article>").addClass("tweet");
+    let $postedTweet = $articleElm.append(data.content.text);
+
+    return $postedTweet;
+}
+
+
+const $tweet = createTweetElement(tweetData);
+
+
+
+
+console.log($tweet); // to see what it looks like
+$('#tweets-container').prepend($tweet);
