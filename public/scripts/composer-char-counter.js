13fc7627f83afa7d8e2859ddce2c9f5bb0b27069
Keishon Ross <keishonross97@gmail.com>
2018-11-07 02:17:00 +0000

    fixed bug where counter is not turning back to black when it reaches above 0

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -7,12 +7,14 @@ $(function() {
 
     
     textArea.on('input', function(event) {
-        console.log("textArea text length: ", this.textLength);
+        // console.log("textArea text length: ", this.textLength);
         const $counter = $(this).parent().children('.counter');
         let curntCharCount = $counter.text(maxChar - this.textLength);
-        
+        //Checking if the characters is less than 0 
         if(curntCharCount.text() <= 0) {
-        $counter.css('color', 'red');
+            $counter.css('color', 'red');
+        } else { //Checking for the else condition
+            $counter.css('color', 'black');
         };
    
     });
