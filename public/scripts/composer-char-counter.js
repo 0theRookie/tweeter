1b77a314b8b39bacb6f720a37a1eda515b140d9e
Keishon Ross <keishonross97@gmail.com>
2018-11-07 02:10:49 +0000

    character count now turns red when it hits 0 and lower

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -6,10 +6,16 @@ $(function() {
     const maxChar = 140;
 
     
-   textArea.on('input', function(event) {
-      console.log("textArea text length: ", this.textLength);
-      const $counter = $(this).parent().children('.counter');
-      $counter.text(maxChar - this.textLength);
+    textArea.on('input', function(event) {
+        console.log("textArea text length: ", this.textLength);
+        const $counter = $(this).parent().children('.counter');
+        let curntCharCount = $counter.text(maxChar - this.textLength);
+        
+        if(curntCharCount.text() <= 0) {
+        $counter.css('color', 'red');
+        };
+   
     });
+
    
 });
