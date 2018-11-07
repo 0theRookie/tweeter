61fb2fe6ed1182f1836f2bb13185ea4dc55fbf3f
Keishon Ross <keishonross97@gmail.com>
2018-11-07 04:57:19 +0000

    the function was unnecessary, set hover event to css

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,8 @@
-$(function () {
-    console.log('DOM Ready');
-    const tweetArticle = $('article.tweet');
-
-    tweetArticle.on('mouseover', function(event) {
-        console.log("moused over");
-    })
+// $(function () {
+//     console.log('DOM Ready');
+//     const tweetArticle = $('article.tweet');
 
 
 
-})
+// })
 
