0f2a3917b689a68c9b660a32bf116d387804e517
Keishon Ross <keishonross97@gmail.com>
2018-12-04 22:55:51 +0000

    fixes bugs relating to compose box not being styled correctly,

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,18 +1,19 @@
 
 $(function() {
     // console.log("DOM ready");
-
-    const textArea = $('textarea');
+    const $form = $('form');
+    const $textArea = $('textarea');
     const maxChar = 140;
 
     
-    textArea.on('input', function(event) {
+    $textArea.on('input', function(event) {
         
         const $counter = $(this).parent().children('.counter');
         let curntCharCount = $counter.text(maxChar - this.textLength);
         //Checking if the characters is less than 0 
         if(curntCharCount.text() <= 0) {
             $counter.css('color', 'red');
+        
         } else { //Checking for the else condition
             $counter.css('color', 'black');
         };
